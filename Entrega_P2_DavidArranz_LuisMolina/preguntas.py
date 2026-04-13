75e74b8711b1eb435fc3a2365daa63a89c6188ab
Tuligambas <161507816+Tuligambas@users.noreply.github.com>
2026-04-13 19:26:15 +0200

    entrega final

---
+++ Entrega_P2_DavidArranz_LuisMolina/preguntas.py
@@ -0,0 +1,285 @@
+"""
+Modulo que contiene diferentes modelos de consulta para la seccion de "trivia".
+"""
+import random
+from typing import List
+from abc import ABC, abstractmethod
+from .operaciones_coleccion import OperacionesEurovision
+
+
+# Clases para encapsular las preguntas y respuestas generadas aleatoriamente
+class Trivia(ABC):
+    """
+    Clase abstracta con los metodos que deben implementar todas las preguntas de trivia.
+    """
+    @abstractmethod
+    def __init__(self, parametros: OperacionesEurovision):
+        # Obligamos a que todos los constructores les pasen un objeto con los parametros aleatorios
+        pass
+
+    @property
+    @abstractmethod
+    def pregunta(self) -> str:
+        """
+        Pregunta que se debe mostrar
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def opciones_invalidas(self) -> List[str]:
+        """
+        Lista de opciones invalidas. Deben ser exactamente 3
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def respuesta(self) -> str:
+        """
+        Respuesta correcta
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def puntuacion(self) -> int:
+        """
+        Puntuacion asociada a la pregunta
+        """
+        pass
+
+    def to_dict(self):
+        # Sorteamos aleatoriamente las respuestas
+        respuestas = [self.respuesta, *self.opciones_invalidas]
+        random.shuffle(respuestas)
+
+        # Funcion que genera la informacion que pasamos al script de trivia en el formato adecuado
+        return {"pregunta": self.pregunta,
+                "correcta": respuestas.index(self.respuesta),
+                "respuestas": respuestas,
+                "puntuacion": self.puntuacion,
+                "tipo": "pregunta"}
+
+
+class PrimerAnyoParticipacion(Trivia):
+    """
+    Pregunta que anyo fue el primero en el que participo un pais seleccionado aleatoriamente
+    """
+
+    def __init__(self, parametros: OperacionesEurovision):
+        #busco utilizando las funciones auxiliares un pais aleatorio sobre el que hacer la pregunta
+        self.pais = parametros.paises_participantes_aleatorios(1)[0]
+
+        #cojo el año de participacion del pais
+        #parecido a numero_peliculas_por_categorias!!!!
+        agregacion = list(parametros.agregacion([
+            {"$unwind": "$concursantes"},
+            {"$match": {"concursantes.pais": self.pais}},
+            {"$group": {
+                "_id": "$concursantes.pais",
+                "primer_anyo": {
+                    "$min": "$anyo"}}}
+        ]))
+        self._respuesta = agregacion[0]["primer_anyo"]
+
+        opciones = parametros.anyo_aleatorio(50)
+        opciones_invalidas = []
+        for anyo in opciones:
+            if anyo != self._respuesta and anyo not in opciones_invalidas:
+                opciones_invalidas.append(anyo)
+            if len(opciones_invalidas) == 3:
+                break
+
+        self._opciones_invalidas = opciones_invalidas
+
+    @property
+    def pregunta(self) -> str:
+        return f"¿En qué año participó por primera vez {self.pais}?"
+
+    @property
+    def opciones_invalidas(self) -> List[str]:
+        return self._opciones_invalidas
+
+    @property
+    def respuesta(self) -> str:
+        return self._respuesta
+
+    @property
+    def puntuacion(self) -> int:
+        """
+        Puntuacion asociada a la pregunta
+        """
+        return 2
+
+
+class CancionPais(Trivia):
+    """
+    Pregunta de que pais es el interprete de una cancion, dada el titulo de la cancion
+    """
+
+    def __init__(self, parametros: OperacionesEurovision):
+        # Obtenemos una participacion para la respuesta
+        participacion_aleatoria = parametros.participacion_aleatoria(1)[0]
+        self._respuesta = participacion_aleatoria["pais"]
+        self._cancion = participacion_aleatoria["cancion"]
+
+        # saco posibles paises y aplico la misma logica que antes
+        opciones = parametros.participacion_aleatoria(50)
+        opciones_invalidas = []
+        for pais in opciones:
+            pais_participante = pais["pais"]
+            if pais_participante != self._respuesta and pais_participante not in opciones_invalidas:
+                opciones_invalidas.append(pais_participante)
+            if len(opciones_invalidas) == 3:
+                break
+
+        self._opciones_invalidas = opciones_invalidas
+
+    @property
+    def pregunta(self) -> str:
+        return f"¿De que país es el intérprete de la canción '{self._cancion}'?"
+
+    @property
+    def opciones_invalidas(self) -> List[str]:
+        return self._opciones_invalidas
+
+    @property
+    def respuesta(self) -> str:
+        return self._respuesta
+
+    @property
+    def puntuacion(self) -> int:
+        """
+        Puntuacion asociada a la pregunta
+        """
+        return 1
+
+
+class MejorClasificacion(Trivia):
+    """
+    Pregunta: ¿Que cancion/pais obtuvo la mejor posicion en un anyo dado?
+
+    Respuesta: las respuestas deben ser de la forma cancion/pais.
+
+    IMPORTANTE: la solucion debe ser unica. Ademas, todos las opciones
+    deben haber participado el mismo anyo.
+    """
+    def __init__(self, parametros: OperacionesEurovision):
+        self._anyo = parametros.anyo_aleatorio(1)[0]
+        agregacion_ordenada = list(parametros.agregacion([
+                {"$match": {"anyo": self._anyo}},
+                {"$unwind": "$concursantes"},
+                {"$project": {
+                    "_id": 0,
+                    "resultado": "$concursantes.resultado",
+                    "respuesta": {"$concat": ["$concursantes.cancion", " / ", "$concursantes.pais"]}
+                }},
+                {"$sort": {"resultado": 1}}
+            ]))
+        self._respuesta = agregacion_ordenada[0]["respuesta"]
+
+        #igual pero sin ordenar para que las 3 opciones invalidas no sean las otras 3 mejores
+        agregacion_desordenada = list(parametros.agregacion([
+                {"$match": {"anyo": self._anyo}},
+                {"$unwind": "$concursantes"},
+                {"$project": {
+                    "_id": 0,
+                    "resultado": "$concursantes.resultado",
+                    "respuesta": {"$concat": ["$concursantes.cancion", " / ", "$concursantes.pais"]}
+                }}
+        ]))
+
+        self._opciones_invalidas = []
+        for i in agregacion_desordenada:
+            opcion = i["respuesta"]
+            if opcion != self.respuesta and opcion not in self._opciones_invalidas:
+                self._opciones_invalidas.append(opcion)
+            if len(self._opciones_invalidas) == 3:
+                break
+
+    @property
+    def pregunta(self) -> str:
+        return f"¿Que canción/país obtuvo la mejor posición en {self._anyo}?"
+
+    @property
+    def opciones_invalidas(self) -> List[str]:
+        return self._opciones_invalidas
+
+    @property
+    def respuesta(self) -> str:
+        return self._respuesta
+
+    @property
+    def puntuacion(self) -> int:
+        return 3
+
+
+class MejorMediaPuntos(Trivia):
+    """
+    Pregunta que pais ha tenido mejor media de resultados en un periodo determinado.
+
+    IMPORTANTE: la solucion debe ser unica.
+    """
+    def __init__(self, parametros: OperacionesEurovision):
+        aux = parametros.anyo_aleatorio(2)
+        if aux[0]<aux[1]:
+            self._anyo_inicial = aux[0]
+            self._anyo_final = aux[1]
+        else:
+            self._anyo_inicial = aux[1]
+            self._anyo_final = aux[0]
+
+        agregacion_ordenada = list(parametros.agregacion([
+            {"$match": {"anyo": {"$gte": self._anyo_inicial, "$lte": self._anyo_final}}},
+            {"$unwind": "$concursantes"},
+            {"$group": {
+                "_id": "$concursantes.pais",
+                "media_resultado": {"$avg": "$concursantes.resultado"}
+            }},
+            {"$project": {
+                "_id": 0,
+                "pais": "$_id",
+                "media_resultado": 1
+            }},
+            {"$sort": {"media_resultado": 1}}  # menor media = mejor
+            ]))
+        self._respuesta = agregacion_ordenada[0]["pais"]
+
+        agregacion_desordenada = list(parametros.agregacion([
+            {"$match": {"anyo": {"$gte": self._anyo_inicial, "$lte": self._anyo_final}}},
+            {"$unwind": "$concursantes"},
+            {"$group": {
+                "_id": "$concursantes.pais",
+                "media_resultado": {"$avg": "$concursantes.resultado"}
+            }},
+            {"$project": {
+                "_id": 0,
+                "pais": "$_id",
+                "media_resultado": 1
+            }}
+        ]))
+        self._opciones_invalidas = []
+        for fila in agregacion_desordenada:
+            opcion = fila["pais"]
+            if opcion != self._respuesta and opcion not in self._opciones_invalidas:
+                self._opciones_invalidas.append(opcion)
+            if len(self._opciones_invalidas) == 3:
+                break
+
+
+    @property
+    def pregunta(self) -> str:
+        return f"¿Qué país quedó mejor posicionado de media entre los años {self._anyo_inicial} y {self._anyo_final}?"
+
+    @property
+    def opciones_invalidas(self) -> List[str]:
+        return self._opciones_invalidas
+
+    @property
+    def respuesta(self) -> str:
+        return self._respuesta
+
+    @property
+    def puntuacion(self) -> int:
+        return 4
---
+++ Entrega_P2_DavidArranz_LuisMolina/videos.py
@@ -0,0 +1,192 @@
+"""
+Modulo para hacer cuestiones de trivia relacionadas con videos de Youtube. Creamos una clase TriviaVideo que extiende
+a Trivia y almacena el id de reproduccion de video
+"""
+
+from abc import ABC, abstractmethod
+from typing import List
+from pathlib import Path
+
+from.operaciones_coleccion import OperacionesEurovision
+from .preguntas import Trivia
+
+
+def extraer_id_url(url) -> str:
+    """
+    Para renderizar el juego, necesitamos extraer el id desde la url del video.
+    Utilizamos expresiones regulares
+    """
+    try:
+        return Path(url).name
+    except:
+        # Return id for Rick Roll
+        return "dQw4w9WgXcQ"
+
+
+class TriviaVideo(Trivia, ABC):
+    """
+    Clase abstracta que contiene los metodos que deben incorporar las preguntas asociadas a videos.
+    """
+    @property
+    @abstractmethod
+    def url(self) -> str:
+        pass
+
+    def to_dict(self):
+        # Modifica el diccionario de Trivia con la url del video
+        # y el tipo "video"
+        super_dict = super().to_dict()
+        super_dict["url"] = self.url
+        # Extraemos el id de la URL
+        super_dict["url_id"] = extraer_id_url(self.url)
+        super_dict["tipo"] = "video"
+        return super_dict
+
+
+class PaisActuacion(TriviaVideo):
+    """
+    ¿Que pais represento la cancion?
+    """
+    def __init__(self, parametros: OperacionesEurovision):
+        participacion_aleatoria = parametros.participacion_aleatoria(1)[0]
+        self._url = participacion_aleatoria["url_youtube"]
+        self._respuesta = participacion_aleatoria["pais"]
+
+        opciones = parametros.paises_participantes_aleatorios(50)
+        opciones_invalidas = []
+        for pais in opciones:
+            if pais != self._respuesta and pais not in opciones_invalidas:
+                opciones_invalidas.append(pais)
+            if len(opciones_invalidas) == 3:
+                break
+
+        self._opciones_invalidas = opciones_invalidas
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def pregunta(self) -> str:
+        return "¿A qué país representó esta canción?"
+
+    @property
+    def opciones_invalidas(self) -> List[str]:
+        return self._opciones_invalidas
+
+    @property
+    def respuesta(self) -> str:
+        return self._respuesta
+
+    @property
+    def puntuacion(self) -> float:
+        return 3
+
+
+class NombreCancion(TriviaVideo):
+    """
+    ¿Cual es el titulo de esta cancion?
+
+    NOTA: para dificultar la respuesta, se deben seleccionar canciones del mismo pais.
+    """
+    def __init__(self, parametros: OperacionesEurovision):
+        participacion_aleatoria = parametros.participacion_aleatoria(1)[0]
+        self._url = participacion_aleatoria["url_youtube"]
+        self._respuesta = participacion_aleatoria["cancion"]
+        pais_participando = participacion_aleatoria["pais"]
+        canciones_pais = list(parametros.agregacion([
+            {"$unwind": "$concursantes"},
+            {"$match": {
+                "concursantes.pais": pais_participando}}, #me quedo solo con las participaciones del pais
+            {"$project":{
+                "_id":0,
+                "cancion": "$concursantes.cancion" #me quedo solo con sus canciones
+            }},
+            {"$group": {
+                "_id": "$cancion" #para evitar repetidos
+            }}
+        ]))
+        opciones_invalidas = []
+        for cancion in canciones_pais:
+            titulo = cancion["_id"]
+            if titulo != self._respuesta and titulo not in opciones_invalidas:
+                opciones_invalidas.append(titulo)
+            if len(opciones_invalidas) == 3:
+                break
+        self._opciones_invalidas = opciones_invalidas
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def pregunta(self) -> str:
+        return "¿Cual es el titulo de esta cancion?"
+
+    @property
+    def opciones_invalidas(self) -> List[str]:
+        return self._opciones_invalidas
+
+    @property
+    def respuesta(self) -> str:
+        return self._respuesta
+
+    @property
+    def puntuacion(self) -> float:
+        return 2
+
+
+class InterpreteCancion(TriviaVideo):
+    """
+    ¿Quien interpreto esta cancion?
+
+    NOTA: para dificultar la respuesta, se deben seleccionar interpretes del mismo pais.
+    """
+
+    def __init__(self, parametros: OperacionesEurovision):
+        participacion_aleatoria = parametros.participacion_aleatoria(1)[0]
+        self._url = participacion_aleatoria["url_youtube"]
+        self._respuesta = participacion_aleatoria["artista"]
+
+        pais_participando = participacion_aleatoria["pais"]
+        artistas_pais = list(parametros.agregacion([
+            {"$unwind": "$concursantes"},
+            {"$match": {
+                "concursantes.pais": pais_participando}},  # me quedo solo con las participaciones del pais
+            {"$project": {
+                "_id": 0,
+                "artista": "$concursantes.artista"  # me quedo solo con sus artistas
+            }},
+            {"$group": {
+                "_id": "$artista"  # para evitar repetidos
+            }}
+        ]))
+
+        opciones_invalidas = []
+        for artista in artistas_pais:
+            nombre = artista["_id"]
+            if nombre != self._respuesta and nombre not in opciones_invalidas:
+                opciones_invalidas.append(nombre)
+            if len(opciones_invalidas) == 3:
+                break
+        self._opciones_invalidas = opciones_invalidas
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def pregunta(self) -> str:
+        return f"¿Qué artista interpretó esta canción?"
+
+    @property
+    def opciones_invalidas(self) -> List[str]:
+        return self._opciones_invalidas
+
+    @property
+    def respuesta(self) -> str:
+        return self._respuesta
+
+    @property
+    def puntuacion(self) -> float:
+        return 4
